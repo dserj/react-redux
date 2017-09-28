ee1b931bd309ec45ba2025ed39e910128ab4c150
serj <serj@msk.bonasource.ru>
2017-09-28 17:37:59 +0300

    thunk modified

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,7 +1,18 @@
 import * as types from './actyonTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course){
+function loadCoursesSuccess(courses){
   return {
-    type: types.CREATE_COURSE, course
+    type: types.LOAD_COURSES_SUCCESS, course: courses
+  };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
   };
 }
