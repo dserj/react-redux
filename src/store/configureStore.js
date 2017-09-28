8b0085c1748552aa1eb23b7bbfa1278b921fd0d2
serj <serj@msk.bonasource.ru>
2017-09-28 16:35:13 +0300

    thunk added

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutable from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 // here we can pass some usefuls for the initial configuration
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutable())
+    applyMiddleware(thunk, reduxImmutable())
   );
 }
