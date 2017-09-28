d7eaa67fb03530205207cac3a276ee30843e5615
serj <serj@msk.bonasource.ru>
2017-09-28 14:56:52 +0300

    webpack hmr fixed

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,27 @@
 import React, { PropTypes } from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: null }
+    };
+  }
   render() {
     return (
       <div>
-        <h1>Courses1</h1>
+        <h1>Courses</h1>
+        <h2>Add course</h2>
+        <input
+          type="text"
+          onChange={this.onTiteChange}
+          value={this.state.course.title} />
+
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
       </div>
     );
   }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,16 +1,23 @@
 import webpack from 'webpack';
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import autoprefixer from 'autoprefixer';
 
 export default {
-  debug: true,
-  // devtool: 'cheap-module-eval-source-map', // described in the course
-  devtool: 'inline-source-map',
-  noInfo: false, // set to true - we can see what webpack is doing
+  //debug: true,
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
+  devtool: 'cheap-module-eval-source-map', // described in the course
+  //devtool: 'inline-source-map',
+  //noInfo: false, // set to true - we can see what webpack is doing
   entry: [
-    'eventsource-polyfill', // necessary for hot reloading with IE
+    //'eventsource-polyfill', // necessary for hot reloading with IE
     // ?reload=true param tells webpack to reload the page if hot reloading fails for some reason
+    './src/webpack-public-path',
+    'react-hot-loader/patch',
     'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-    path.resolve(__dirname, 'src/index')
+    path.resolve(__dirname, 'src/index.js')
   ],
   target: 'web', // we can change it so, our code can run in node
   // webpack won't generate any actual files for our development. It will serve files from memory
@@ -23,19 +30,45 @@ export default {
     contentBase: path.resolve(__dirname, 'src')
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'), // Tells React to build in either dev or prod modes. https://facebook.github.io/react/downloads.html (See bottom)
+      __DEV__: true
+    }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({     // Create HTML file that includes references to bundled CSS and JS.
+      template: 'src/index.ejs',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      inject: true
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+      noInfo: true, // set to false to see a list of every file being bundled.
+      options: {
+        sassLoader: {
+          includePaths: [path.resolve(__dirname, 'src', 'scss')]
+        },
+        context: '/',
+        postcss: () => [autoprefixer]
+      }
+    })
   ],
 
   // this section tells webpack what file types it should handle
   module: {
-    loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ['style', 'css']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    rules: [
+      {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader'},
+      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
+      {test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'},
+      {test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=[name].[ext]'},
+      {test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'},
+      {test: /(\.css|\.scss|\.sass)$/, loaders: ['style-loader', 'css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap']}
     ]
   }
 };
