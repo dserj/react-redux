9fe2e1465dda4fa775f7d2393eda5ffdb9c86eb2
serj <serj@msk.bonasource.ru>
2017-09-28 15:24:02 +0300

    configure store

---
+++ src/store/configureStore.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers';
+import reduxImmutable from 'redux-immutable-state-invariant';
+
+// here we can pass some usefuls for the initial configuration
+export default function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(reduxImmutable())
+  );
+}
