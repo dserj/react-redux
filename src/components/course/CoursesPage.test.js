9fe96eafd5c008f0cd5196684d39f2f986ba5158
dserj <d_sergej@mail.ru>
2017-10-02 22:18:06 +0300

    test using React Test Utils

--- src/components/course/CoursesPage.test.js
+++ src/components/course/CoursesPage.test.js
@@ -3,9 +3,9 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import CourseForm from './CourseForm';
 
-function setup() {
+function setup(saving) {
   let props = {
-    course: {}, saving: false, errors: {},
+    course: {}, saving, errors: {},
     onSave: () => {},
     onChange: () => {}
   };
@@ -28,5 +28,17 @@ describe('Course Form via React Test Utils', () => {
 
     let [ h1 ] = output.props.children; // destructuring the array
     expect(h1.type).toBe('h1');
+  });
+
+  it('Save button is labeled "Save" when not saving', () => {
+    const { output } = setup(false);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe('Save');
+  });
+  
+  it('Save button is labeled "Saving..." when saving', () => {
+    const { output } = setup(true);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe('Saving...');
   });
 });
